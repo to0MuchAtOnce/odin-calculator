c62b63e0f62989bd2d0a493f162a27896263ad21
Tom lewis <tom.lewis488@gmail.com>
2023-12-01 15:53:09 +0100

    Update ui

---
+++ index.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+console.log(add(2, 5));
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+console.log(subtract(9, 6));
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+console.log(multiply(2, 2));
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+console.log(divide(10, 2));
